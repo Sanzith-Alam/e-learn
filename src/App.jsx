701a3b557c8013040854079d4d65e6fbcf38ce1c
Sanzith Alam <121573768+Sanzith@users.noreply.github.com>
2025-02-25 13:04:38 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <main className="overflow-x-hidden bg-white text-dark">
+    <main className="overflow-x-hidden bg-black text-dark">
       <Hero />
       <Services />
       <Banner />
