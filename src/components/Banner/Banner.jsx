e8e411ad270bde6b99f91c3eb043a5d050944f74
Sanzith Alam <121573768+Sanzith@users.noreply.github.com>
2025-02-25 12:56:57 +0600

    Update Banner.jsx

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -9,7 +9,7 @@ import { motion } from "framer-motion";
 const Banner = () => {
   return (
     <section>
-      <div className="bg-black container py-14 md:py-24 grid grid-cols-1 md:grid-cols-2 gap-8 space-y-6 md:space-y-0">
+      <div className=" container py-14 md:py-24 grid grid-cols-1 md:grid-cols-2 gap-8 space-y-6 md:space-y-0">
         {/* Banner Image */}
         <div className="flex justify-center items-center">
           <motion.img
